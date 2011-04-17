01600e2c00e0e0a7bee3fe59c176372582929a52
Mendel Gusmao <mendelsongusmao@gmail.com>
2011-04-16 22:59:16 -0300

    Setting up Windows environment'

--- syntaxtest.sh
+++ syntaxtest.sh
@@ -1,4 +1,8 @@
+export PHP4_BIN="/usr/local/php4/bin/php"
+export PHP5_BIN="/usr/local/php4/bin/php"
+
 for filename in ./*.php
 do
-php -l $filename
+PHP 4 :: `$PHP4_BIN -l $filename`
+PHP 5 :: `$PHP5_BIN -l $filename`
 done;
