1db40a4d6c45484e8d7022b8d1fa35bc691669ff
Mendel Gusmao <mendelsongusmao@gmail.com>
2011-09-02 15:03:37 -0300

    Dismembered Bridge::callback() in Call::callback() and CallsQueue::process_callbacks(); Refactored tests. Still studying on how to implement a Runner based on proc_open()

---
+++ tests/basic.sh
@@ -0,0 +1,3 @@
+rm temp/*
+php4 tests/basic/frontend.php
+ls temp/*.php5b | xargs tools/php5b
---
+++ tools/php5b.php
@@ -0,0 +1,8 @@
+<?php
+
+    $php5b = $_SERVER["argv"][1];
+    
+    if (file_exists)
+        echo unserialize(f)
+
+?>
