beb4ea5ed19fd746e5c956cec050a55244547577
Mendel Gusmao <mendelsongusmao@gmail.com>
2011-09-12 18:54:46 -0300

    Once FilePersistence is fully working now, enabled informal tests

--- tests/basic/backend.php
+++ tests/basic/backend.php
@@ -10,16 +10,18 @@
     function compare_php_version ($version) { return "From $version to " . PHP_VERSION; }
 
     class Backend {
+        function __construct () { echo __CLASS__, " constructed\n"; }
         function backend_md5($input) { return md5($input); }
     }
 
     class Backend2 {
         private $str;
-        function __construct($params) { $this->str = $params["str"]; }
+        function __construct($params) { echo __CLASS__, " constructed\n"; $this->str = $params["str"]; }
         function backend_sha1() { return sha1($this->str); }
         function backend_raw() { return $this->str; }
     }
 
     class Backend3 {
+        function __construct () { echo __CLASS__, " constructed\n"; }
         static function backend_version() { return "Backend is " . PHP_VERSION; }
     }
--- tests/basic/frontend.php
+++ tests/basic/frontend.php
@@ -8,11 +8,11 @@
     $calls = new CallsQueue;
     
     $calls->enqueue(
-        new Call("compare_php_version", PHP_VERSION, null, "var_dump")/*,
+        new Call("compare_php_version", PHP_VERSION, null, "var_dump"),
         new Call(array("Backend", "backend_md5"), "The World", null, "var_dump"),
         new Call(array("Backend2", "backend_sha1"), null, array("str" => "The World"), "var_dump"),
         new Call(array("Backend3", "::backend_version"), null, null, "var_dump"),
-        new Call(array("&Backend2", "backend_raw"), null, null, "var_dump")*/
+        new Call(array("&Backend2", "backend_raw"), null, null, "var_dump")
     );
 
     $bridge = new Bridge(new FilePersistence, $backend, $calls);
