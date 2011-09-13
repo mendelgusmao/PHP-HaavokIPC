7894bcc8753ff6360b88a671b5ffbfe9069cf1fa
Mendel Gusmao <mendelsongusmao@gmail.com>
2011-09-13 18:07:56 -0300

    Code cleanup

--- phpgr.conf.php
+++ phpgr.conf.php
@@ -3,11 +3,8 @@
     /* Backend binary */
     define("PHPGR_BACKEND_BIN", "/usr/bin/php");
 
-    /* Define whether backend's STDOUT will be sent to the frontend */
-    define("PHPGR_NO_BACKEND_OUTPUT", false);
-
     /* FilePersistence config */
-    define("PHPGR_EXT", ".php5b");
+    define("PHPGR_EXT", ".persistence");
     define("PHPGR_TMP", dirname(__FILE__) . "/temp/");
 
     /* Logging config */
@@ -15,7 +12,6 @@
     define("PHPGR_LOG", true);
 
     /* Memcache config */
-    define("PHPGR_USE_MEMCACHE", false);
     define("PHPGR_MEMCACHED", "127.0.0.1");
     define("PHPGR_MEMCACHEDP", 11211);
     
