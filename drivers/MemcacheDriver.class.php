9e8b9704fd3da2f43b32a91f3583e4a89cb57805
Mendel Gusmao <mendelsongusmao@gmail.com>
2011-09-19 19:53:13 -0300

    Renamed *Persistence classes to *Driver and grouped them under drivers directory

---
+++ drivers/MemcacheDriver.class.php
@@ -0,0 +1,85 @@
+<?php
+
+    /**
+     * Class responsible for the input and output of the data used by PHP-Ghetto-IPC
+     * using Memcached
+     *
+     * @author Mendel Gusmao
+     *
+     */
+    class MemcacheDriver {
+
+        var $name = "Memcache";
+
+        var $id;
+        var $source;
+        var $data;
+        var $valid;
+
+        /**
+         * Constructor extender
+         */
+        function initialize ($id, $fallback_source = null) {
+
+            $this->valid = false;
+
+            if (class_exists("Memcache")) {
+
+                $this->source = new Memcache;
+
+                if ($this->source->addServer(PHPGI_MEMCACHED, PHPGI_MEMCACHEDP)) {
+                    $this->valid = true;
+                }
+                else {
+                    trigger_error("PHP-Ghetto-IPC::MemcachePersistence::initialize: Couldn't connect to memcached at " . PHPGI_MEMCACHED . ":" . PHPGI_MEMCACHEDP, E_USER_ERROR);
+                }
+
+            }
+            else {
+                trigger_error("PHP-Ghetto-IPC::MemcachePersistence::initialize: Memcache is not enabled.", E_USER_ERROR);
+            }
+
+            return $this->valid;
+        }
+
+        /**
+         * Sets the data to the source
+         *
+         * @param mixed $data The data to be set
+         */
+        function set ($data) {
+
+            return $this->source->set($this->id, $data, 1);
+
+        }
+
+        /**
+         * Gets the data from the source
+         *
+         * @return mixed $data The data
+         */
+        function get () {
+
+            return $this->source->get($this->id);
+
+        }
+
+        function delete () {
+
+            return $this->source->delete($this->id);
+
+        }
+
+        /**
+         * Verify if the source is valid
+         * @return bool
+         */
+        function valid () {
+
+            return $this->valid;
+
+        }
+
+    }
+
+?>
---
+++ drivers/StdIODriver.class.php
@@ -0,0 +1,84 @@
+<?php
+    /**
+     * Class responsible for the input and output of the data used by PHP-Ghetto-IPC
+     * using STDIN/STDOUT
+     *
+     *
+     * @author Mendel Gusmao
+     *
+     */
+    class StdIODriver {
+
+        var $name = "StdIO";
+        
+        var $id;
+        var $runner;
+        var $data;
+        var $valid;
+
+        /**
+         * Constructor extender
+         */
+        function initialize ($id) {
+
+            $this->id = $id;
+            $this->valid = false;
+
+            if ($this->stdin_descriptor = fopen($this->stdin_file, "r") &&
+                $this->stdout_descriptor = fopen($this->stdout_file, "w"))
+                $this->valid = true;	
+                
+            return $this->valid;
+        }
+
+        /**
+         * Sets the data to the source
+         *
+         * @param mixed $data The data to be set
+         */
+        function set ($data) {
+
+            $data = serialize($data);
+            fwrite($this->stdout_descriptor, $data);
+			
+        }
+
+        /**
+         * Gets the data from the source file
+         *
+         * @return mixed $data The data
+         */
+        function get () {
+
+            $data = "";
+
+            while ($temp = fread($this->stdin_descriptor, 1024))
+                $data .= $temp;
+
+            return unserialize($data);
+        }
+
+        /**
+         * Delete the source file
+         *
+         * @return boolean
+         */
+        function delete () {
+
+            return true;
+            
+        }
+
+        /**
+         * Verify if the source is valid
+         * @return bool
+         */
+        function valid () {
+
+            return $this->valid & is_array(@fstat($this->descriptor));
+            
+        }
+
+    }
+
+?>
