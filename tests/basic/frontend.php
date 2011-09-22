c600d3208e062768d837bccaf18a01aa0f853ac3
Mendel Gusmao <mendelsongusmao@gmail.com>
2011-09-22 18:14:39 -0300

    Updated informal tests

--- tests/basic/frontend.php
+++ tests/basic/frontend.php
@@ -7,6 +7,7 @@
     $calls = new CallsQueue;
     
     $calls->enqueue(
+        new Call("fopen", array(__FILE__, "r"), null, "var_dump"),
         new Call("compare_php_version", PHP_VERSION, null, "var_dump"),
         new Call(array("Backend", "backend_md5"), "The World", null, "var_dump"),
         new Call(array("Backend2", "backend_sha1"), null, array("str" => "The World"), "var_dump"),
@@ -23,3 +24,5 @@
     echo "#################################################################\n";
     echo $ipc->output2;
     echo "#################################################################\n";
+    echo print_r($ipc->errors);
+    echo "-----------------------------------------------------------------\n";
