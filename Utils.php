287238aba967521d24a993d0b8c97554c3312e0e
Mendel Gusmao <mendelsongusmao@gmail.com>
2011-09-28 15:39:02 -0300

    More information in error message

--- Utils.php
+++ Utils.php
@@ -3,8 +3,9 @@
     function phpgi_trigger_error ($class, $function, $message, $error_level = E_USER_ERROR) {
 
         $end = PHPGI_IS_BACKEND ? "Backend" : "Frontend";
+        $version = PHP_VERSION;
 
-        trigger_error("PHP-Ghetto-IPC ({$end})::{$class}::{$function}: {$message}");
+        trigger_error("PHP-Ghetto-IPC ({$end} [{$version}])::{$class}::{$function}: {$message}", $error_level);
 
     }
 
