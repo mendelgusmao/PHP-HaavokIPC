27997db1c32783f13e738dd74a9438de61c70c5e
Mendel Gusmao <mendelsongusmao@gmail.com>
2011-10-05 10:51:09 -0300

    Updating test

--- tests/basic/frontend.php
+++ tests/basic/frontend.php
@@ -12,11 +12,17 @@
         new Call(array("Backend", "backend_md5"), "The World", null, "var_dump"),
         new Call(array("Backend2", "backend_sha1"), null, array("str" => "The World"), "var_dump"),
         new Call(array("Backend3", "::backend_version"), null, null, "var_dump"),
-        new Call(array("&Backend2", "backend_raw"), null, null, "var_dump")
+        new Call(array("&Backend2", "backend_raw"), null, null, "callback", 1)
     );
 
+    function callback () {
+        $args = func_get_args();
+        echo "Callback called in frontend\n";
+        print_r($args);
+    }
+
     $ipc = new GhettoIPC(new FileDriver, $backend, $calls);
-    
+    $ipc->set_export_options(PHPGI_EXPORT_SERVER);
     $ipc->execute(true); // execute callbacks
     
     echo "#################################################################\n";
@@ -24,5 +30,5 @@
     echo "#################################################################\n";
     echo $ipc->output2;
     echo "#################################################################\n";
-    echo print_r($ipc->errors);
+    print_r($ipc->errors);
     echo "-----------------------------------------------------------------\n";
